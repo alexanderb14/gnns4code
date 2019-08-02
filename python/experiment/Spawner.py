d735bdfebe23c141373ed20eef09e3e173c23989
Alexander Brauckmann <alexander.brauckmann@tu-dresden.de>
2019-08-02 15:45:53 +0200

    Implement Spawner.

---
+++ python/experiment/Spawner.py
@@ -0,0 +1,42 @@
+import argparse
+import concurrent.futures
+import os
+import subprocess
+import sys
+
+
+def print_process_stdout_continuously(process, prefix):
+    while True:
+        line = process.stdout.readline()
+        if not line:
+            break
+        print(str(prefix) + ': ' + str(line.rstrip(), 'utf-8'))
+
+        line = process.stderr.readline()
+        if not line:
+            break
+        print(str(prefix) + ': ' + str(line.rstrip(), 'utf-8'), file=sys.stderr)
+    process.wait()
+    print(prefix + ' RETURNCODE: ' + str(process.returncode))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--command')
+    parser.add_argument('--iterations')
+
+    args = parser.parse_args()
+
+    iterations = range(0, int(args.iterations))
+
+    def fnc(iteration):
+        command = args.command.split(' ')
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=os.getcwd())
+        print_process_stdout_continuously(process, iteration)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        executor.map(fnc, iterations)
+
+if __name__ == '__main__':
+    main()
